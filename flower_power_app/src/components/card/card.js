715f564490f226c14b2a22fe68661a4a088a1a1d
cimpoes.andrei.alexandru@gmail.com <cimpoes.andrei.alexandru@gmail.com>
2018-12-31 12:56:49 +0200

    React app setup

---
+++ flower_power_app/src/components/card/card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Card (props){
+    console.log('Card', props);
+    return (
+        <div className="content-card">
+         <h1>{props.name}</h1>
+         <button onClick={() => props.handleClick(props.name)}>Click name</button>
+        </div>
+    )
+}
+Card.propTypes={
+    name: PropTypes.string,
+    handleClick: PropTypes.func,
+}
+export default Card;
